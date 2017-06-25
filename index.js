1b38c8314ccc89289f2e32dde456ca9d99fc751e
Tobias Gurtzick <magic@wizardtales.com>
2017-06-25 15:09:56 +0200

    add family capabilities

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Base = require('db-migrate-pg').base,
+    util = require('util'),
     pg = require('pg'),
     log,
     type,
@@ -9,6 +10,51 @@ var CockroachDriver = Base.extend({
   init: function(connection, schema, intern) {
 
     this._super(connection, schema, intern);
+  },
+
+  _applyExtensions: function(options) {
+
+    var families = {},
+        firstFamily,
+        sql = [];
+
+    Object.keys(options).forEach(function(key) {
+
+      var option = options[key];
+
+      if(option.family && typeof(option.family) === 'string') {
+
+        families[option.family] = families[option.family] || [];
+        families[option.family].push(key);
+
+        if(option.primaryKey === true)
+          firstFamily = option.family;
+      }
+    });
+
+    if(firstFamily) {
+
+      sql.push(util.format('FAMILY %s (%s)',
+        key,
+        families[firstFamily].join(', ')
+      ));
+    }
+
+    Object.keys(families).forEach(function(key) {
+
+      if(key !== firstFamily) {
+
+        sql.push(util.format('FAMILY %s (%s)',
+          key,
+          families[key].join(', ')
+        ));
+      }
+    });
+
+    if(sql.length === 0)
+      return '';
+
+    return ', ' + sql.join(', ');
   }
 });
 
