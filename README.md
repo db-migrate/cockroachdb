79c6509e7915113941e56622b21fab88c03e1b4f
Tobias Gurtzick <magic@wizardtales.com>
2017-06-25 13:23:11 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+[![Build Status](https://travis-ci.org/db-migrate/cockroachdb.svg?branch=master)](https://travis-ci.org/db-migrate/cockroachdb)
+[![Dependency Status](https://david-dm.org/db-migrate/cockroachdb.svg)](https://david-dm.org/db-migrate/cockroachdb)
+[![devDependency Status](https://david-dm.org/db-migrate/cockroachdb/dev-status.svg)](https://david-dm.org/db-migrate/cockroachdb#info=devDependencies)
+
+
 # cockroachdb
-A cockroachdb driver.
+cockroachdb driver for db-migrate
+
+## Installation
+
+```
+npm install db-migrate-cockroachdb
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+var Base = require('db-migrate-pg'),
+    pg = require('pg'),
+    log,
+    type,
+    Promise = require('bluebird');
+
+var CockroachDriver = Base.extend({
+
+  init: function(connection, schema, intern) {
+
+    this._super(connection, schema, intern);
+  }
+});
+
+
+exports.connect = function(config, intern, callback) {
+
+    log = intern.mod.log;
+    type = intern.mod.type;
+
+    if (config.native) { pg = pg.native; }
+    var db = config.db || new pg.Client(config);
+    callback(null, new PgDriver(db, config.database, intern));
+};
